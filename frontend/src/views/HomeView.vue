8f7110316d140cffd2f1c896c0d6c56a8706263a
johnny.asumbra <johnny@sdsolutions.com.ph>
2023-12-18 06:34:21 +0800

    base initial of the system

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <h1 class="font-bold text-2xl">Hello Wrold</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
